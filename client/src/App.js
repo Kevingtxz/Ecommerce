7c2bdd5f860fc0844afacb97fe1163abdab8753d
Kevin <kevingtxz@gmail.com>
2021-03-14 23:23:59 -0300

    3

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import Login from './pages/auth/Login.js'
 import Register from './pages/auth/Register.js'
+import RegisterComplete from './pages/auth/RegisterComplete.js'
 import Home from './pages/Home.js'
 import Header from './components/nav/Header.js'
 
@@ -9,10 +13,12 @@ const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer/>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/register/complete' component={RegisterComplete} />
       </Switch>
     </>
   )
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase'
+import firebase from "firebase/app"
+import "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyBt2-DRzitIbBoHb9MAMGcaScyN-MFGvBo",
@@ -8,7 +9,7 @@ const firebaseConfig = {
     messagingSenderId: "332215396492",
     appId: "1:332215396492:web:246ea9272d35b2df1473a9"
 };
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 export const auth = firebase.auth()
-export const googleAuthProvider = new firebase.auth.googleAuthProvider()
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
